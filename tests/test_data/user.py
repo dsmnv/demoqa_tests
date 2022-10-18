640f830c3e37dd2cff0a25479f80233cb3312c32
dsmnv <dmitry.d.sem@gmail.com>
2022-10-18 21:29:32 +0300

    Отредактировал юзера

--- tests/test_data/user.py
+++ tests/test_data/user.py
@@ -1,38 +1,19 @@
 from dataclasses import dataclass
-from enum import Enum
 from typing import Tuple
-
-
-class Subject(Enum):
-    History = 'History'
-    Maths = 'Maths'
-    Physics = 'Physic'
-
-
-class Hobby(Enum):
-    Sports = '1'
-    Reading = '2'
-    Music = '3'
-
-
-class Gender(Enum):
-    Male = 'Male'
-    Female = 'Female'
-    Other = 'Other'
 
 
 @dataclass
 class user:
-    gender: Gender
-    name: str = 'Dmitry'
-    last_name: str = 'Sem'
+    gender: str = 'Male'
+    firstname: str = 'Dmitry'
+    lastname: str = 'Sem'
     email: str = 'demotest@tete.com'
-    mobile: str = '9570000000'
+    phone: str = '9570000000'
     birth_day: str = '5'
     birth_month: str = 'November'
     birth_year: str = '1995'
-    subjects: Tuple[Subject] = (Subject.History, Subject.Maths, )
-    hobbies: Tuple[Hobby] = (Hobby.Sports, Hobby.Reading)
+    subject1: str = 'English'
+    subject2: str = 'Maths'
     current_address: str = 'Saint - P'
     picture_file: str = 'donut.png'
     state: str = 'Haryana'
