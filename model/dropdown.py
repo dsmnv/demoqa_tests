6f29a03c54b780fe146511db976f945d21ae8e08
dsmnv <dmitry.d.sem@gmail.com>
2022-10-19 23:31:51 +0300

    Добавил дропдаун

--- model/dropdown.py
+++ model/dropdown.py
@@ -1,12 +1,14 @@
 from selene import have
 from selene.support.shared import browser
+from selene import command
 
 
 def select_dropdown(element, option):
+    element.perform(command.js.scroll_into_view)
     element.click()
     browser.all('[id^=react-select][id*=-option-]').by(
         have.exact_text(option)
-    ).click()
+    ).first.click()
 
 
 def select_by_typing(element, option):
